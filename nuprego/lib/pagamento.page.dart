881376664277c7d4b82a1a048ec80b9c6a1c69da
ThiagoMDM <thiagomdm.ti@gmail.com>
2021-07-21 11:05:05 -0300

    Criação da classe Pagamento, front

---
+++ nuprego/lib/pagamento.page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class PagamentoPage extends StatefulWidget {
+  @override
+  _PagamentoPageState createState() => _PagamentoPageState();
+}
+
+class _PagamentoPageState extends State<PagamentoPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      backgroundColor: Colors.blue,
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Center(
+          child: Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ),
+      }
+  }
